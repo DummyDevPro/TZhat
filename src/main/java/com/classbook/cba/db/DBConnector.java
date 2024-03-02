414e8ee148701d739b503bb0aa5f424c87205746
DummyDevPro <htun@osf.ac.jp>
2024-03-02 12:04:07 +0900

    add future async for db accessing

--- src/main/java/com/classbook/cba/db/DBConnector.java
+++ src/main/java/com/classbook/cba/db/DBConnector.java
@@ -2,6 +2,9 @@ package com.classbook.cba.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class DBConnector {
 	private static Connection connection;
@@ -13,11 +16,17 @@ public class DBConnector {
 	private static Connection createConnection() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection conn = DriverManager.getConnection(
+			// Add async for server delay
+			ExecutorService threadpool = Executors.newCachedThreadPool();
+			Future<Connection> futureTask = threadpool.submit(() -> DriverManager.getConnection(
 					"jdbc:mysql://mysql-14395797-dummy-hmm-2024.a.aivencloud.com:12571/cb_app_db?sslmode=require",
-					"avnadmin", "AVNS_9GyEO8aN8-iRO0_Abuo");
+					"avnadmin", "AVNS_9GyEO8aN8-iRO0_Abuo"));
+			while (!futureTask.isDone()) {
+				System.out.println("FutureTask is not finished yet...");
+			}
+			Connection conn = futureTask.get();
 			connection = conn;
-			new Thread().wait(6000);
+			threadpool.shutdown();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
